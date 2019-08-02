36fe5f4fd3de8f2cfa8dbff1c12790ab3a2021d3
Victor <victorcr1@icloud.com>
2019-08-02 18:08:59 +0200

    Finished the implementation of the login feature

--- StudyMar/StudyMarApp/urls.py
+++ StudyMar/StudyMarApp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views 
 
+app_name = 'StudyMarApp'
 urlpatterns = [
 	
 	path('', views.login, name='login'),	
--- StudyMar/StudyMarApp/views.py
+++ StudyMar/StudyMarApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from .models import User
+from django.urls import reverse
 
 
 
@@ -16,7 +18,27 @@ def login(request):
 
 
 def auth(request):
-	return HttpResponse("You are in the authentication view")
+	# return HttpResponse("You are in the authentication view")
+	# username = request.POST['username']
+	try:
+		user = User.objects.get(username__iexact=request.POST['username'])
+	except (KeyError, User.DoesNotExist):
+		template = loader.get_template('StudyMarApp/login.html')
+		context = {'error_message':"There is no such username"}
+		return HttpResponse(template.render(context, request))
+	else:
+		password = user.password
+		ins_password = request.POST['password']
+		if (password == ins_password):
+			return HttpResponseRedirect(reverse('StudyMarApp:home', args=(user.username,)))
+		else:
+			context = {'error_message':"The password for the user %s is incorrect" % user.username}
+			template = loader.get_template('StudyMarApp/login.html')
+			return HttpResponse(template.render(context, request))
+
+		# return HttpResponse("You are in the authentication view for the user %s." % user)
+
+
 
 def home(request, username):
 	return HttpResponse("You are in the home page of the user %s." % username)
